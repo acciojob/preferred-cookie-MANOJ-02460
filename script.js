3a14714d90e351b1938cec1ece2676aa8dda6da0
MANOJ-02460 <131439674+MANOJ-02460@users.noreply.github.com>
2026-09-07 15:11:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -7,17 +7,17 @@ submitBtn.addEventListener("click", function(event){
 	let fontsizeInput= document.getElementById("fontsize");
 	let fontcolorInput = document.getElementById("fontcolor");
 
-	let fontsizeValue =fontsizeInput.value;
-	let fontcolorValue = fontcolorInput.value;
+	let fontsize =fontsizeInput.value;
+	let fontcolor = fontcolorInput.value;
 
-	if(fontsizeValue.trim() !== ""  && fontcolorValue.trim() !== ""){
+	if(fontsize.trim() !== ""  && fontcolor.trim() !== ""){
 		const daysToExpire = 7;
 		const date = new Date();
 		date.setTime(date.getTime()+(daysToExpire *24 *60 *60 *1000));
 		const expires = "expires=" + date.toUTCString();
 
-		document.cookies = "fontsizeValue="+encodeURIComponent(fontsizeValue)+"; "+expires+"; path=/";
-		document.cookies = "fontcolorValue="+encodeURIComponent(fontcolorValue)+"; "+expires+"; path=/";
+		document.cookies = "fontsize="+encodeURIComponent(fontsize)+"; "+expires+"; path=/";
+		document.cookies = "fontcolor="+encodeURIComponent(fontcolor)+"; "+expires+"; path=/";
 		alert("Both cookies are saved successfully");
 	}else{
 		alert("Please fill out both fields")
